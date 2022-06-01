88259c6b11cdaee1e23722e206cf7da9a8c5d273
rodrigoqgomez <rodrigoqgomez@gmail.com>
2022-06-01 17:24:27 -0500

    git push origin main

--- app/src/main/java/com/example/residencia/MainActivity.kt
+++ app/src/main/java/com/example/residencia/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.residencia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton1=findViewById<Button>(R.id.alumno)
+       // boton1.setOnClickListener { val intent=intent(this,login::cl startActivity(login)) }
     }
 }
+
+
---
+++ app/src/main/java/com/example/residencia/login.java
@@ -0,0 +1,4 @@
+package com.example.residencia;
+
+public class login {
+}
